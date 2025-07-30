c8b7cb91f9e2aee33ccecb98c3733d261ba40e18
Alec Velitskiy <alecvelitskiy@gmail.com>
2025-07-29 20:59:13 -0400

    Array example

--- src/day01/ExtraFile.java
+++ src/day01/ExtraFile.java
@@ -1,6 +1,21 @@
 package day01;
+import java.util.Arrays;
 
 public class ExtraFile {
 
     // this is an extra file
+
+    public static void main(String[] args) {
+
+        int [] num = new int[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+
+        System.out.println(Arrays.toString(num)); // [1, 2, 3]
+
+
+
+
+    }
 }
